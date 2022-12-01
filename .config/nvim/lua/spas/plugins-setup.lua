fd0f2798438ad733537783a5cc649f90a6056308
SpasZahariev <spaszahariev54@gmail.com>
2022-12-01 22:27:38 +0000

    adding refactoring extension

--- .config/nvim/lua/spas/plugins-setup.lua
+++ .config/nvim/lua/spas/plugins-setup.lua
@@ -102,6 +102,15 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- for extract variable/extract method shortcut. Go go Prime!
+	use({
+		"ThePrimeagen/refactoring.nvim",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	})
+
 	-- Debugger Adapter Protocol
 	use("mfussenegger/nvim-dap")
 	use("rcarriga/nvim-dap-ui")
--- .config/nvim/lua/spas/plugins/treesitter.lua
+++ .config/nvim/lua/spas/plugins/treesitter.lua
@@ -16,7 +16,7 @@ treesitter.setup({
 	autotag = { enable = true },
 	-- configure rainbow parenthesis,
 	rainbow = {
-		enable = true,
+		enable = false,
 		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
